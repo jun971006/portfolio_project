39ae3380d5647ee9e754c1a5c13683f0eecddd48
jun971006 <wnstjrdlrj@gmail.com>
2019-05-25 07:14:58 +0000

    change main.py, view/db/connect_mongo.py, view/templates/login.html,signup.html, view/userAPI.py

--- view/db/connect_mongo.py
+++ view/db/connect_mongo.py
@@ -5,8 +5,8 @@ class ConnectDB():
 	def __init__(self):
 		with open("view/db/mongoDB.json") as Json:
 			user_doc = json.loads(Json.read())
-			self.mongoURL = str("mongodb+srv://%s:%s%s"%(user_doc['MongoID'], user_doc['MongoPW'], user_doc['mongoURL']))
-			self.client = pymongo.MongoClient(self.mongoURL)
+			self.MongoURL = str("mongodb+srv://%s:%s%s"%(user_doc['MongoID'], user_doc['MongoPW'], user_doc['MongoURL']))
+			self.client = pymongo.MongoClient(self.MongoURL)
 			self.db = pymongo.database.Database(self.client, 'Cluster0')
 
 	def close(self):
