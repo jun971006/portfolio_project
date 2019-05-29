36af31aeefc0319eedb97730224733f285e3f3ec
jun971006 <wnstjrdlrj@gmail.com>
2019-05-29 03:59:59 +0000

    change [view/db]connect_mongo.py, postsDAO.py

---
+++ view/db/postsDAO.py
@@ -0,0 +1,33 @@
+import pymongo
+from bson.objectid import objectid
+
+class Posts():
+	def __init__(self, db):
+		self.posts = pymongo.collection.Collectio(db, 'Posts')
+
+	def postCreate(self,postDict):
+		try:
+			obj_id = self.posts.insert_one(postDict).inserted_id
+			return obj_id
+		except:
+			return False
+
+	def postDelete(self, obj_id):
+		try:
+			self.posts.delete_one({"_id":ObjectId(obj_id)})
+			return True
+		except:
+			return False
+	def postUpdate(self,postDict):
+		self.posts.find_and_modify(
+			{"_id": ObjectId(postDict["obj_id"])},
+			{"$set":{"postTitle":postDict["postTitle"],"postContent":postDict["postContent"]}},
+			upsert=False
+			)
+		return True
+	def getAllposts(self):
+		try:
+			 result = self.posts.find({})
+			 return result
+		except:
+			retrun False
